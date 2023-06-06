f344ae20c0ae80a145543466f1c339a22346e429
Lim Jun Ming <limjunming2@gmail.com>
2023-06-06 18:02:31 +0800

    retrieved data

--- cybercrusaders/src/index.js
+++ cybercrusaders/src/index.js
@@ -1,12 +1,12 @@
-// index.js file acts as the entry point for your React application's rendering process and 
+// index.js file acts as the entry point for your React application's rendering process and
 // establishes the connection between your React components and the HTML DOM.
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/App.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/App.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />

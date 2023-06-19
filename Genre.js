de402cfd800ab66804b257b3064b335094941f30
Lim Jun Ming <limjunming2@gmail.com>
2023-06-19 13:53:07 +0800

    filtering

---
+++ Genre.js
@@ -0,0 +1,23 @@
+import "./styles/Genre.css";
+
+const Genre = ({ filterByGenres }) => {
+  return (
+    <div className="genre-list">
+      <ul>
+        <strong className="genre">Genre</strong>
+        <li onClick={() => filterByGenres("Action")}>Action</li>
+        <li onClick={() => filterByGenres("Adenture")}>Adventure</li>
+        <li onClick={() => filterByGenres("Fighting")}>Fighting</li>
+        <li onClick={() => filterByGenres("MOBA")}>MOBA</li>
+        <li onClick={() => filterByGenres("Fighting")}>Fighting</li>
+        <li onClick={() => filterByGenres("Platform")}>Platform</li>
+        <li onClick={() => filterByGenres("RPG")}>RPG</li>
+        <li onClick={() => filterByGenres("Sports")}>Sports</li>
+        <li onClick={() => filterByGenres("Shooter")}>Shooter</li>
+        <li onClick={() => filterByGenres("Racing")}>Racing</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Genre;

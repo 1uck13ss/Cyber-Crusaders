ca37cdeaae72232a9c35dbdf09c31041024d1cce
KahJyun <kahjyun98@gmail.com>
2023-07-09 23:14:38 +0800

    frontpagedone

--- utils/validation1.js
+++ utils/validation1.js
@@ -2,6 +2,6 @@ import * as Yup from 'yup';
 
 export const validationSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string().required('Password is required'),
+  password: Yup.string().min(6, 'Password must at least have 6 characters').required('Password is required'),
   //mobileNumber: Yup.string().required('Mobile Number is required'),
 });
